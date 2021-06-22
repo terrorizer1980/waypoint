930db225780875577cd4d29b2520afab97685b91
Brian Cain <bcain@hashicorp.com>
2021-06-22 16:46:02 -0700

    Add debug message for outcome 2

--- internal/server/singleprocess/poll.go
+++ internal/server/singleprocess/poll.go
@@ -109,6 +109,7 @@ func (s *service) runPollQueuer(
 
 		if ctx.Err() != nil {
 			// Outcome (2) above
+			log.Debug("context cancelled for poll queuer, returning from poll loop ctx")
 			return
 		}
 
